e5687ff320211b18b75f421f6e862d18d0d76392
Nathan Hill <nateohill@gmail.com>
2019-05-21 16:34:41 -0700

    Added a essay that the student has to save using closures

---
+++ Toy-Problems/closures.js
@@ -0,0 +1,14 @@
+/**
+ * Uh oh, looks like someone is going to accidentally overwrite your essay.
+ * Find out how to save your essay without changing it's variable name by using a closure in the save function.
+ */
+
+let essay = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+function save() {
+  // Your code goes here;
+}
+
+save();
+
+essay = 'Consectetur adipiscing ipsum dolor sit amet.';
